dfdcb5468dda0035f3ec08191e0c248e70da5d29
shomffl <showatanabe0226@gmail.com>
2021-11-03 12:43:19 +0900

    commit

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, redirect, url_for, render_template, Markup
-from werkzeug.utils import secure_filename
+from werkzeug.utils import  secure_filename
 import os
 import shutil
 import pykakasi
-from PIL import Image
 from split import SplitOriginal
 from get_rgb import GetRgb, CompareColors, ConnectImage
 import cv2
@@ -21,15 +20,14 @@ class Kakashi:
         return cls.conv.do(japanese)
 
 UPLOAD_FOLDER = "./static/images/"
-ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
 
-def allowed_file(filename):
-    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -40,38 +38,42 @@ def index():
 @app.route("/result", methods=["GET", "POST"])
 def result():
     if request.method == "POST":
-        # ファイルの存在と形式を確認
-        if "file" not in request.files:
-            print("File doesn't exist!")
-            return redirect(url_for("index"))
         file = request.files["file"]
-        if not allowed_file(file.filename):
-            print(file.filename + ": File not allowed!")
-            return redirect(url_for("index"))
 
         # ファイルの保存
-        # shutil.rmtree(UPLOAD_FOLDER)
-        # os.mkdir(UPLOAD_FOLDER)
         filename = secure_filename(file.filename)  # ファイル名を安全なものに
         filepath = os.path.join(UPLOAD_FOLDER+"upload_image_files", filename)
         file.save(filepath)
 
-        # 画像の読み込み
-        image = Image.open(filepath)
-        image = image.convert("RGB")
-        image = image.resize((400, 400))
-        new_path= os.path.join(UPLOAD_FOLDER+"upload_image_files", "resize_image.png")
-        image.save(new_path)
+        shutil.rmtree(f"{UPLOAD_FOLDER}split_images_file")
+        os.mkdir(f"{UPLOAD_FOLDER}split_images_file")
 
-        SplitOriginal(8, "./static/images/upload_image_files/resize_image.png","./static/images/split_image_files/").split_image()
-        read_original = GetRgb("split_image_files")
+        # 画像の読み込み
+        image = cv2.imread(filepath)
+        height = image.shape[0]
+        width = image.shape[1]
+        longer_one = max(height, width)
+        adjust_per = 400 / longer_one
+        adjust_height = int(round(height * adjust_per, -1))
+        adjust_width = int(round(width * adjust_per, -1))
+
+        resize_image = cv2.resize(image, dsize=(adjust_width, adjust_height))
+        new_path= os.path.join(UPLOAD_FOLDER+"upload_image_files", "resize_image.png")
+        cv2.imwrite(new_path, resize_image)
+
+        split = SplitOriginal(5, f"{UPLOAD_FOLDER}upload_image_files/resize_image.png",f"{UPLOAD_FOLDER}split_images_file/")
+        length = split.split_image()
+        height_num = length[0]
+        width_num = length[1]
+
+        read_original = GetRgb("split_images_file")
         read_material = GetRgb("simple_images/small_material_files")
         cul = CompareColors(read_original.get_rgb(), read_material.get_rgb()).compare()
-        create = ConnectImage(8, 400, cul,"simple_images")
+        create = ConnectImage(height_num, width_num, cul,"simple_images")
         create.connect_image()
 
 
-        return render_template("result.html", result=Markup(result), filepath="./static/images/download_original_files/mosaic_image.png")
+        return render_template("result.html", height = int(adjust_height * 1.5), width = int(adjust_width * 1.5), filepath="./static/images/download_original_files/mosaic_image.png")
 
 
 
--- get_rgb.py
+++ get_rgb.py
@@ -51,21 +51,18 @@ class CompareColors():
 #画像を結合する
 class ConnectImage():
 
-    def __init__(self, size, length, min_numbers, course_name):
-        self.size = size
-        self.length = length
+    def __init__(self, height_num, width_num, min_numbers, course_name):
+        self.height_num = height_num
+        self.width_num = width_num
         self.min_numbers = min_numbers
         self.course_name = course_name
 
     def connect_image(self):
-        side = self.length
-        img_num = int(side / self.size)
-
-        list1 = np.array_split(self.min_numbers, img_num)
+        list1 = np.array_split(self.min_numbers, self.height_num)
         big_img = []
         for i in list1:
             small_img = []
-            for j in range(img_num):
+            for j in range(self.width_num):
                 read = cv2.imread(f"./static/images/{self.course_name}/big_material_files/canvas{i[j]}.png")
                 small_img.append(read)
             big_img.append(small_img)
